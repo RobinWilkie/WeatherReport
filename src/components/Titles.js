40a239940dfea0d2f0567ae67d4c6273365314dd
Robin Wilkie <robinwilkieglasgow@hotmail.com>
2018-12-12 10:15:12 +0000

    first commit

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather Report</h1>
+        <p>Display weather conditions for your area</p>
+      </div>
+    );
+  }
+}
+
+export default Titles;
