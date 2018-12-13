6205a2af0ee7b70736703da9872f23c8f4862920
Robin Wilkie <robinwilkieglasgow@hotmail.com>
2018-12-13 16:53:52 +0000

    added stying

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,10 +2,18 @@ import React from "react";
 
 const Weather = props => {
   return (
-    <div>
-      {props.humidity && <p>Humidity: {props.humidity}%</p>}
+    <div
+      style={{
+        width: "18rem",
+        backgroundColor: "#1d1d1d",
+        color: "#fff",
+        borderColor: "#1d1d1d"
+      }}
+    >
+      {props.wind && <h5>Summary</h5>}
       {props.wind && <p>Wind Speed: {props.wind}mps</p>}
       {props.clouds && <p>Cloud Cover: {props.clouds}%</p>}
+      {props.humidity && <p>Humidity: {props.humidity}%</p>}
       {props.error && <p>{props.error}</p>}
     </div>
   );
--- src/components/WeatherFuture.js
+++ src/components/WeatherFuture.js
@@ -5,14 +5,24 @@ const WeatherFuture = props => {
   return (
     <div>
       {props.city && props.country && <h4>Tomorrow's Weather</h4>}
-      {props.temperature1 && <p>Temperature: {props.temperature1}&deg; C</p>}
-      {props.humidity1 && <p>Humidity: {props.humidity1}%</p>}
-      {props.wind1 && <p>Wind Speed: {props.wind1}mps</p>}
-      {props.clouds1 && <p>Cloud Cover: {props.clouds1}%</p>}
-      {props.description1 && <p>{props.description1}</p>}
-      {props.icon1 && (
-        <img className="weather-icon" src={weatherIcon} alt="weather icon" />
-      )}
+      <div className="d-flex">
+        <div className="d-flex flex-column mr-2 p-2 border-right border-white">
+          {props.icon1 && (
+            <img
+              className="weather-icon"
+              src={weatherIcon}
+              alt="weather icon"
+            />
+          )}
+          {props.temperature1 && <h4>{props.temperature1}&deg; C</h4>}
+        </div>
+        <div>
+          {props.humidity1 && <p>Humidity: {props.humidity1}%</p>}
+          {props.wind1 && <p>Wind Speed: {props.wind1}mps</p>}
+          {props.clouds1 && <p>Cloud Cover: {props.clouds1}%</p>}
+          {props.description1 && <p>{props.description1}</p>}
+        </div>
+      </div>
       {props.error && <p>{props.error}</p>}
     </div>
   );
